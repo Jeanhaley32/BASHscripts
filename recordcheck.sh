dfd3ae3dfc168bf0d86df37d5d607780fd13ff61
Jean <jeanhaley32@gmail.com>
2023-11-09 14:05:27 -0500

    Update recordcheck.sh
    changing some hard coded values to variables defined at the top

--- recordcheck.sh
+++ recordcheck.sh
@@ -1,20 +1,21 @@
 #! /bin/bash
 # Script created to check when a TXT record for a subdomain became available. 
 # Saving, because I may want to use some of this in a future script. 
-SUBDOMAIN=<enter your target here>
+subdomain=<enter your target here>
+sleeptime=3
 start=$(date +%s)
 while true; do
-  result=$(dig TXT $SUBDOMAIN +short)
+  result=$(dig TXT $subdomain +short)
   if [ -z "$result" ]; then
     now=$(date +%s)
     duration=$((now - start))
-    echo "The TXT record does not exist yet. Checking again in 3 seconds... Running for $duration seconds."
+    echo "The TXT record does not exist yet. Checking again in $sleeptime seconds... Running for $duration seconds."
   else
     now=$(date +%s)
     duration=$((now - start))
     echo "Success! The TXT record exists. Total running time: $duration seconds."
     break
   fi
-  sleep 3
+  sleep $sleeptime
   clear
 done
